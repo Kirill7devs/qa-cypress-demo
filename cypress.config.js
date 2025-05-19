58ecbee52078a70ed60fb7f18930d38032dc361a
Kirill <43032208+kirill-the-qa@users.noreply.github.com>
2025-05-19 16:15:00 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {email: TEST_EMAIL, password: TEST_PASSWORD} = process.env
+const { email: TEST_EMAIL, password: TEST_PASSWORD, baseUrl: BASE_URL } = process.env
+
 export default defineConfig({
   e2e: {
     specPattern: [
       "cypress/e2e/01_SuperAdmin/*.cy.js",
       "cypress/e2e/02_AdminPanel/*.cy.js",
     ],
-    baseUrl: "http://localhost:8000",
+    baseUrl,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
