73bfd0274222f8b642562d9b4731ddae649831ae
Kirill <43032208+kirill-the-qa@users.noreply.github.com>
2025-06-16 20:35:36 +0300

    Delete cypress/01_SuperAdmin directory

--- cypress/01_SuperAdmin/01_login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Login', () => {
-  before(() => {
-    cy.login()
-  })
-  it('create session and redirect to offers page', () => {
-    cy.visit('/superadmin/offers')
-    cy.url().should('include', '/offers')
-    })
-  })
